c789446c9cf2154ccbf1735f3af740723a68d4a4
beeinger <sulecki.maciek@gmail.com>
2023-11-10 11:27:52 +0000

    WIP Draft MMR

--- src/mmr/mmrs/draft.rs
+++ src/mmr/mmrs/draft.rs
@@ -1,11 +1,9 @@
 use std::rc::Rc;
 
-use uuid::Uuid;
-
 use crate::{
     hasher::Hasher,
     mmr::{MmrMetadata, MMR},
-    store::Store,
+    store::memory::InMemoryStore,
 };
 
 /// A tuple of the size at which the MMR is stacked and the MMR itself.
@@ -15,10 +13,45 @@ impl<H> MMR<H>
 where
     H: Hasher + Clone,
 {
-    pub fn new_draft(store: Rc<dyn Store>, hasher: H, sub_mmrs_metadata: SizesToMMRs<H>) -> Self {
-        let mmr_id = format!("draft:{}", Uuid::new_v4());
-        let mmr = MMR::new_stacked(store, hasher, Some(mmr_id), sub_mmrs_metadata);
+    pub fn start_draft(&mut self) -> DraftMMR<H> {
+        let store = InMemoryStore::default();
+        let store = Rc::new(store);
+        let hasher = self.hasher.clone();
 
-        mmr
+        let mut sub_mmrs = self.sub_mmrs.clone();
+        sub_mmrs.push((self.elements_count.get(), self.get_metadata()));
+
+        let draft_mmr = MMR::new_stacked(store.clone(), hasher, None, sub_mmrs);
+
+        DraftMMR {
+            store,
+            ref_mmr: self,
+            mmr: draft_mmr,
+        }
+    }
+}
+
+// TODO finish below
+#[allow(dead_code)]
+pub struct DraftMMR<'a, H>
+where
+    H: Hasher,
+{
+    store: Rc<InMemoryStore>,
+    ref_mmr: &'a mut MMR<H>,
+    pub mmr: MMR<H>,
+}
+
+impl<H> DraftMMR<'_, H>
+where
+    H: Hasher + Clone,
+{
+    pub fn discard(self) {
+        self.store.clear();
+    }
+
+    // TODO - implement this
+    pub fn commit(self) {
+        panic!("Not implemented")
     }
 }
