956e7ea1f68ebec405b9be2b72bdedd673aa3909
beeinger <sulecki.maciek@gmail.com>
2023-11-10 12:07:30 +0000

    Initial commit implementation

--- src/mmr/mmrs/draft.rs
+++ src/mmr/mmrs/draft.rs
@@ -1,4 +1,4 @@
-use std::rc::Rc;
+use std::{collections::HashMap, rc::Rc};
 
 use crate::{
     hasher::Hasher,
@@ -31,8 +31,6 @@ where
     }
 }
 
-// TODO finish below
-#[allow(dead_code)]
 pub struct DraftMMR<'a, H>
 where
     H: Hasher,
@@ -50,8 +48,23 @@ where
         self.store.clear();
     }
 
-    // TODO - implement this
     pub fn commit(self) {
-        panic!("Not implemented")
+        let mut to_set = HashMap::new();
+        for (key, value) in self.store.store.read().iter() {
+            let (_, key, sub_key) =
+                MMR::<H>::decode_store_key(key).expect("Could not decode store key");
+            let full_key = MMR::<H>::encode_store_key(&self.ref_mmr.mmr_id, key, sub_key);
+
+            to_set.insert(full_key, value.to_string());
+        }
+
+        println!("✅ to_set: {:?}", to_set);
+
+        self.ref_mmr
+            .store
+            .set_many(to_set)
+            .expect("Could not apply draft to MMR");
+
+        self.store.clear();
     }
 }
