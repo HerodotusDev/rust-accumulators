0be217af0b518de71dd4b8e98aa61ac1dbdf8873
Pia <gayeongparkk@gmail.com>
2024-01-15 21:15:39 +0900

    MOREE TESTS

--- tests/mmr/draft.rs
+++ tests/mmr/draft.rs
@@ -261,9 +261,10 @@ async fn append_block_range_poseidon_aggregator() {
         .calculate_root_hash(&draft_bag, elements_count)
         .expect("Failed to calculate root hash");
     assert_eq!(
-        draft_root, "0x1a2be63d0560708d3eb87319be0442016ba8757557da8009096e95c4b0682d9",
-        "draft_root "
+        draft_root,
+        "0x1a2be63d0560708d3eb87319be0442016ba8757557da8009096e95c4b0682d9"
     );
+
     // block 9734440
     draft
         .mmr
@@ -280,12 +281,43 @@ async fn append_block_range_poseidon_aggregator() {
         7,
         "elements_count  should be 7"
     );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    assert_eq!(
+        draft_root,
+        "0x1006c333b41230ee484977b481e0e4e530f454a6d14902ce593ed2dbf649a25"
+    );
+
     // block 9734441
     draft
         .mmr
         .append("0x7f6d47c24e8723a6d6cf4ef089df0bd3ec710d5448b696e47b037109a1d04ce".to_string())
         .await
         .expect("Failed to append");
+    assert_eq!(
+        draft.mmr.leaves_count.get().await.unwrap(),
+        5,
+        "leaves_count  should be 5"
+    );
+    assert_eq!(
+        draft.mmr.elements_count.get().await.unwrap(),
+        8,
+        "elements_count  should be 8"
+    );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    assert_eq!(
+        draft_root,
+        "0x4f05ebf1a932fdc481d43eb577ad326e5a8c743fbc8624eb98010b65f8c5b89"
+    );
 
     // block 9734442
     draft
@@ -293,30 +325,135 @@ async fn append_block_range_poseidon_aggregator() {
         .append("0x38e557fbc306cbcb5964a503014b375db68a0c6786fd9c6ffc5cdd14b6c9dfc".to_string())
         .await
         .expect("Failed to append");
+    assert_eq!(
+        draft.mmr.leaves_count.get().await.unwrap(),
+        6,
+        "leaves_count  should be 6"
+    );
+    assert_eq!(
+        draft.mmr.elements_count.get().await.unwrap(),
+        10,
+        "elements_count  should be 10"
+    );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    assert_eq!(
+        draft_root,
+        "0x13bdeaf86b66a03cb316c62d475a7e8d037d30ee7b5d52ff1b13f2fb951b527"
+    );
+
     // block 9734443
     draft
         .mmr
         .append("0x54aa6067e8c4f6bcd7c47cf7900df1d960098177e186f0c15b6a7544491b539".to_string())
         .await
         .expect("Failed to append");
+    assert_eq!(
+        draft.mmr.leaves_count.get().await.unwrap(),
+        7,
+        "leaves_count  should be 7"
+    );
+    assert_eq!(
+        draft.mmr.elements_count.get().await.unwrap(),
+        11,
+        "elements_count  should be 11"
+    );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    assert_eq!(
+        draft_root,
+        "0x763a6f33c8cf7b1411cf23910ddba74841dc4b52c73ecfb57ddc40160c78fc6"
+    );
+
     // block 9734444
     draft
         .mmr
         .append("0x2f185aa16419cad043ddb0b75a7ba0c4233d51b7fee31f1ad6680f5c2b53677".to_string())
         .await
         .expect("Failed to append");
+    assert_eq!(
+        draft.mmr.leaves_count.get().await.unwrap(),
+        8,
+        "leaves_count  should be 8"
+    );
+    assert_eq!(
+        draft.mmr.elements_count.get().await.unwrap(),
+        15,
+        "elements_count  should be 15"
+    );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    assert_eq!(
+        draft_root,
+        "0x7abaf3802dee5c46f80d30101a3882645070d0968758b2a9b7a3bc5e1a059fa"
+    );
+
     // block 9734445
     draft
         .mmr
         .append("0x3ff20a1d65c24d07ebedb2de39c0a27e67808b49d1544e8ef972da1d24da302".to_string())
         .await
         .expect("Failed to append");
+    assert_eq!(
+        draft.mmr.leaves_count.get().await.unwrap(),
+        9,
+        "leaves_count  should be 9"
+    );
+    assert_eq!(
+        draft.mmr.elements_count.get().await.unwrap(),
+        16,
+        "elements_count  should be 16"
+    );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    assert_eq!(
+        draft_root,
+        "0x27f8b7a2ed6d1290833c6fa587d564a8810ee1925b1b2cdfde8da1cefdee57b"
+    );
+
     // block 9734446
     draft
         .mmr
         .append("0x437048beb7e0b3f95fb670e34ac4bd2f32acf6a8ad3eb5fc08682f285ad805b".to_string())
         .await
         .expect("Failed to append");
+    assert_eq!(
+        draft.mmr.leaves_count.get().await.unwrap(),
+        10,
+        "leaves_count  should be 10"
+    );
+    assert_eq!(
+        draft.mmr.elements_count.get().await.unwrap(),
+        18,
+        "elements_count  should be 18"
+    );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    assert_eq!(
+        draft_root,
+        "0x2c02006787d05a482e0a19771a82c353d65a8eff7e69e1be9ae2219d0400951"
+    );
+
     // block 9734447
     draft
         .mmr
@@ -376,6 +513,16 @@ async fn append_block_range_keccak_aggregator() {
         3,
         "elements_count  should be 3"
     );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    assert_eq!(
+        draft_root,
+        "0xffbb02de013f6837d8e0da5f4215c53634c32a4f5eb2520f26a1d6d2f615db72"
+    );
 
     // block 9734439
     draft
@@ -394,6 +541,16 @@ async fn append_block_range_keccak_aggregator() {
         4,
         "elements_count  should be 4"
     );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    assert_eq!(
+        draft_root,
+        "0xaeb642d0f47f806382c66494ccf42c7d37eb3e09ba507a3b842e2a080c745200"
+    );
 
     // block 9734440
     draft
@@ -411,12 +568,43 @@ async fn append_block_range_keccak_aggregator() {
         7,
         "elements_count  should be 7"
     );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    assert_eq!(
+        draft_root,
+        "0xdae951c569985cea6033958972846338710ba372aef365053428d1eccfe5e5ce"
+    );
+
     // block 9734441
     draft
         .mmr
         .append("0x09ab9ad1513282a5c1e1b4c15436aee479e9759712ebe6e5dbb02411537633e1".to_string())
         .await
         .expect("Failed to append");
+    assert_eq!(
+        draft.mmr.leaves_count.get().await.unwrap(),
+        5,
+        "leaves_count  should be 5"
+    );
+    assert_eq!(
+        draft.mmr.elements_count.get().await.unwrap(),
+        8,
+        "elements_count  should be 8"
+    );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    assert_eq!(
+        draft_root,
+        "0xd4675f556d04ea6828165e6ad778f3162978588890061692189a55002d93572a"
+    );
 
     // block 9734442
     draft
@@ -424,30 +612,140 @@ async fn append_block_range_keccak_aggregator() {
         .append("0x5cb8bb916e22e6ab4c0fca4bebc13b05dcaaa7eccacd7636b755d944de4e9217".to_string())
         .await
         .expect("Failed to append");
+    assert_eq!(
+        draft.mmr.leaves_count.get().await.unwrap(),
+        6,
+        "leaves_count  should be 6"
+    );
+    assert_eq!(
+        draft.mmr.elements_count.get().await.unwrap(),
+        10,
+        "elements_count  should be 10"
+    );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    // TODO: TS-ACCUMULATOR is 0x70c01463d822d2205868c5a46eefc55658828015b83e4553c8462d2c6711d0e0
+    assert_eq!(
+        draft_root,
+        "0x1a0a347398081822baeed647ee46c1a50e406133341a0de3f33bb7805092d20d"
+    );
+
     // block 9734443
     draft
         .mmr
         .append("0x0b756461f355b8fb1a6dfdfe5d943f7c037c62b99e806a579500a8a73821e250".to_string())
         .await
         .expect("Failed to append");
+    assert_eq!(
+        draft.mmr.leaves_count.get().await.unwrap(),
+        7,
+        "leaves_count  should be 7"
+    );
+    assert_eq!(
+        draft.mmr.elements_count.get().await.unwrap(),
+        11,
+        "elements_count  should be 11"
+    );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    // TODO: TS-ACCUMULATOR is 0x7d0011a4256839263340fb483eb9fe3f6ce8506c9cc39699d8c1a65d8f34257a
+    assert_eq!(
+        draft_root,
+        "0xca529894efcbddf50b068eccd23d451e081053bc9492d5f69db1164ed4f63b85"
+    );
+
     // block 9734444
     draft
         .mmr
         .append("0x3965b0ccf016b56564129ab0f96400c3a84a8e6fa5d25327a6a1762901ee00e9".to_string())
         .await
         .expect("Failed to append");
+    assert_eq!(
+        draft.mmr.leaves_count.get().await.unwrap(),
+        8,
+        "leaves_count  should be 8"
+    );
+    assert_eq!(
+        draft.mmr.elements_count.get().await.unwrap(),
+        15,
+        "elements_count  should be 15"
+    );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    // TODO: TS-ACCUMULATOR is 0x961d2a731654c2d9027c787a9296c66f841d1ee4a13abfdf7a83b70fd7217060
+    assert_eq!(
+        draft_root,
+        "0x35671dfbd86539afa71aaf4a813550d18bfc36b1e91a5c88554e5b947de510a7"
+    );
+
     // block 9734445
     draft
         .mmr
         .append("0xbe9e359d2632091546be983f8b6488012d607d56c05599c9347fdfdbd86c1b3f".to_string())
         .await
         .expect("Failed to append");
+    assert_eq!(
+        draft.mmr.leaves_count.get().await.unwrap(),
+        9,
+        "leaves_count  should be 9"
+    );
+    assert_eq!(
+        draft.mmr.elements_count.get().await.unwrap(),
+        16,
+        "elements_count  should be 16"
+    );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    // TODO: TS-ACCUMULATOR is 0x4226038dc6fba77fa92ce5d63a49945266065914571c59ef85bdf184eee6dc36
+    assert_eq!(
+        draft_root,
+        "0x27bb48900f6889477589097c26b821aaba4b709b8ea10a5a871ff59f161ea98c"
+    );
+
     // block 9734446
     draft
         .mmr
         .append("0xe9112c401620687b34b0fc6108f35242d32ff37914e302c423e9134851573f65".to_string())
         .await
         .expect("Failed to append");
+    assert_eq!(
+        draft.mmr.leaves_count.get().await.unwrap(),
+        10,
+        "leaves_count  should be 10"
+    );
+    assert_eq!(
+        draft.mmr.elements_count.get().await.unwrap(),
+        18,
+        "elements_count  should be 18"
+    );
+    let elements_count = draft.mmr.elements_count.get().await.unwrap();
+    let draft_bag = draft.mmr.bag_the_peaks(Some(elements_count)).await.unwrap();
+    let draft_root = draft
+        .mmr
+        .calculate_root_hash(&draft_bag, elements_count)
+        .expect("Failed to calculate root hash");
+    // TODO: TS-ACCUMULATOR is 0xc5cce3ec5640e0165df5cf8aa5897eb7b9b54b6c4a17d13e0a007b12cfc223cd
+    assert_eq!(
+        draft_root,
+        "0xfa8d8951eb33ddd3fcc414328df9c0406bdb0f1de47d7302dd0e40b552d5af19"
+    );
+
     // block 9734447
     draft
         .mmr
@@ -472,6 +770,7 @@ async fn append_block_range_keccak_aggregator() {
         .expect("Failed to calculate root hash");
 
     //TODO: onchain root should be 0xc87c3ba0942e428ad5432078aa7bb0b9d423616a3a1c8c7fc27b546a81465aaf
+    //TODO: TS-ACCUMULATOR is 0x4654b1a9b7311b0b896ada391a9481db2c0756d9c0f32658facff9eec32cd18b
     assert_eq!(
         draft_root,
         "0x3833c0ee0a0f3b2d8fa8597c49eed0e53054463fc9ecf05150a188c85142050b"
